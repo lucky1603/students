0db7ad972581480a69373019df47ce5893545c3e
Sinisa Ristic <sinisa.ristic@gmail.com>
2019-03-21 13:45:07 +0100

    Najnoviji eksport baze

--- database/studenti.sql
+++ database/studenti.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 07, 2018 at 01:54 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2019 at 01:44 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,6 +32,7 @@ CREATE TABLE `kursevi` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL COMMENT 'Referenca na tablu studenata',
   `predmet_id` int(11) NOT NULL COMMENT 'Referenca na tabelu  predmeta',
+  `aktivnost` int(11) DEFAULT NULL COMMENT 'U poenima',
   `prisustvo` int(11) DEFAULT NULL COMMENT 'U poenima',
   `broj_casova` int(11) DEFAULT NULL COMMENT 'U danima',
   `samostalni_zadaci` int(11) DEFAULT NULL COMMENT 'Poeni',
@@ -46,19 +47,24 @@ CREATE TABLE `kursevi` (
   `poeni_ukupno_do_usmenog` int(11) DEFAULT NULL COMMENT 'Ukupan broj poena.',
   `poeni_zbir` int(11) DEFAULT NULL COMMENT 'Ukupan zbir poena.',
   `ocena` int(11) DEFAULT NULL,
-  `napomene` text
+  `napomene` text,
+  `skolska_godina` int(11) DEFAULT NULL,
+  `datum_pocetka` date DEFAULT NULL,
+  `datum_okoncanja` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `kursevi`
 --
 
-INSERT INTO `kursevi` (`id`, `student_id`, `predmet_id`, `prisustvo`, `broj_casova`, `samostalni_zadaci`, `kolokvijum_1_poeni`, `kolokvijum_2_poeni`, `kolokvijum_1_datum`, `kolokvijum_2_datum`, `pismeni_datum`, `pismeni_poeni`, `usmeni_datum`, `usmeni_poeni`, `poeni_ukupno_do_usmenog`, `poeni_zbir`, `ocena`, `napomene`) VALUES
-(1, 8, 1, 12, 110, 0, 56, 67, '2017-11-21', '2017-12-05', '0000-00-00', 0, '0000-00-00', NULL, 0, 0, 0, 'nisa specijalno'),
-(2, 9, 1, 12, 33, NULL, 67, 89, '2017-11-22', '2017-12-14', '0000-00-00', 0, '0000-00-00', NULL, 0, 0, 0, NULL),
-(3, 13, 1, 0, 0, NULL, 45, 77, '2017-11-23', '2017-12-21', '0000-00-00', 0, '0000-00-00', NULL, 0, 0, 0, NULL),
-(4, 14, 1, 0, 0, NULL, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', NULL, 0, 0, 0, NULL),
-(5, 14, 2, 0, 0, NULL, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', NULL, 0, 0, 0, NULL);
+INSERT INTO `kursevi` (`id`, `student_id`, `predmet_id`, `aktivnost`, `prisustvo`, `broj_casova`, `samostalni_zadaci`, `kolokvijum_1_poeni`, `kolokvijum_2_poeni`, `kolokvijum_1_datum`, `kolokvijum_2_datum`, `pismeni_datum`, `pismeni_poeni`, `usmeni_datum`, `usmeni_poeni`, `poeni_ukupno_do_usmenog`, `poeni_zbir`, `ocena`, `napomene`, `skolska_godina`, `datum_pocetka`, `datum_okoncanja`) VALUES
+(16, 25, 1, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2019-03-21', NULL),
+(20, 13, 1, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-10-20', NULL),
+(21, 13, 2, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-10-20', NULL),
+(22, 24, 1, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-10-20', NULL),
+(24, 8, 1, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-10-20', NULL),
+(26, 8, 2, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-10-20', NULL),
+(27, 25, 2, 0, 0, NULL, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', 0, '0000-00-00', 0, NULL, 0, NULL, '', NULL, '2018-02-21', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,8 +86,35 @@ CREATE TABLE `predmeti` (
 --
 
 INSERT INTO `predmeti` (`id`, `sifra`, `ime`, `opis`, `fond_casova`, `profesor`) VALUES
-(1, '0302.11', 'Osnovi taktike i gasenja pozara.', 'Kratak opis', 123, 'Barbara Vidakovic'),
+(1, '0302.11', 'Osnovi taktike i gaÅ¡enja poÅ¾ara', 'Kratak opis', 123, 'Barbara Vidakovic'),
 (2, '0303.11', 'Fizika', 'Osnove fizike', 150, 'Barbara Vidakovic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rokovi`
+--
+
+CREATE TABLE `rokovi` (
+  `Id` bigint(20) UNSIGNED NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Description` text NOT NULL,
+  `DateEnding` date NOT NULL,
+  `DateBeginning` date NOT NULL,
+  `Year` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rokovi`
+--
+
+INSERT INTO `rokovi` (`Id`, `Name`, `Description`, `DateEnding`, `DateBeginning`, `Year`) VALUES
+(1, 'Januarsko - Februarski', '', '2019-03-01', '2019-01-24', '2018/2019'),
+(2, 'Aprilski', '', '2019-04-25', '2019-03-25', '2018/2019'),
+(3, 'Junski', '', '2019-06-28', '2019-06-04', '2018/2019'),
+(4, 'Septembarski', '', '2018-09-06', '2018-08-19', '2018/2019'),
+(5, 'Oktobar 1', '', '2019-10-04', '2019-09-16', '2018/2019'),
+(6, 'Oktobar II', '', '2019-11-27', '2019-10-27', '2018/2019');
 
 -- --------------------------------------------------------
 
@@ -105,11 +138,10 @@ CREATE TABLE `studenti` (
 --
 
 INSERT INTO `studenti` (`id`, `ime`, `prezime`, `broj_indeksa`, `email`, `image`, `firma`, `telefon`) VALUES
-(8, 'Sinisa', 'Ristic', '34234234', 'sinisa.ristic@gmail.com', '/img/studenti/mala jaza.jpg', '', ''),
-(9, 'Barbara', 'Vidakovic', '3412341234', 'barbaravid@yahoo.co.uk', '/img/studenti/barbara.png', '', ''),
-(13, 'Jovan', 'Jovanovic', '2342341234', 'jovanjov@gmail.com', '/img/studenti/Patrick McCutcheon.jpg', '', ''),
-(14, 'Mira', 'Markovic', '2342141234', 'mmark@gmail.com', '/img/studenti/Nadja Spitzer.jpg', '', ''),
-(16, 'sfasdf', 'asdfasdfa', '5234523452', 'mma@gmail.com', '/img/studenti/Viola Gauci.jpg', '', '');
+(8, 'SiniÅ¡a', 'RistiÄ‡', '34234234', 'sinisa.ristic@gmail.com', '/img/studenti/mala jaza.jpg', '', ''),
+(13, 'Jovan', 'JovanoviÄ‡', '2342341234', 'jovanjov@gmail.com', '/img/studenti/Patrick McCutcheon.jpg', '', ''),
+(24, 'Leny', 'Krawitz', '123412341', 'leny@gmail.com', '/img/temp/Bert-Arjan Millenaar.png', '', ''),
+(25, 'Petar', 'PetroviÄ‡', '32345-19', 'petar.petrovic@mail.rs', '/img/studenti/2.jpg', '', '');
 
 --
 -- Indexes for dumped tables
@@ -132,6 +164,12 @@ ALTER TABLE `predmeti`
   ADD UNIQUE KEY `sifra_uk` (`sifra`);
 
 --
+-- Indexes for table `rokovi`
+--
+ALTER TABLE `rokovi`
+  ADD UNIQUE KEY `Id` (`Id`);
+
+--
 -- Indexes for table `studenti`
 --
 ALTER TABLE `studenti`
@@ -147,17 +185,26 @@ ALTER TABLE `studenti`
 -- AUTO_INCREMENT for table `kursevi`
 --
 ALTER TABLE `kursevi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
 --
 -- AUTO_INCREMENT for table `predmeti`
 --
 ALTER TABLE `predmeti`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `rokovi`
+--
+ALTER TABLE `rokovi`
+  MODIFY `Id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `studenti`
 --
 ALTER TABLE `studenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
 --
 -- Constraints for dumped tables
 --
